272c9df4b909daaf4a0c53c3a83b331e67038a33
PriyeshGangrade26 <priyeshgangrade786@gmail.com>
2023-06-16 16:17:50 +0530

    Created Todo Web Application

--- server/server.js
+++ server/server.js
@@ -23,6 +23,11 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// Root route handler
+app.get("/", (req, res) => {
+  res.send("To-Do-App Server");
+});
+
 // Routes
 app.use("/user", userRoutes);
 app.use("/todo", todoRoutes);
